609e708f89de7885bf8efaaf51deec214b405d05
joao <jucamatheus13@gmail.com>
2024-08-02 10:16:50 -0300

    alterado

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-import express from "express"
+import express from 'express';
+const servidor = express();
 
-console.log("aoba")
 
-let a = Number(ler())
-let b = Number(ler())
+servidor.get('/calculadora/somar/:n1/:n2', (req, resp) => {
+    let n1 = Number(req.params.n1);
+    let n2 = Number(req.params.n2);
 
-let c = a+b
+    let soma = n1 + n2;
 
-console.log(`${a}+${b}=${c}`)
+    resp.send({
+        soma: `${n1}+${n2}=${soma}` 
+    });
+})
+
+
+servidor.get('/calculadora/subtrair', (req, resp) => {
+    let n1 = Number(req.query.numero1);
+    let n2 = Number(req.query.n2);
+
+    let sub = n1 - n2;
+
+    resp.send('A subtração é ' + sub);
+})
+
+
+servidor.listen(5050, () => console.log('API subiu na porta 5050!'));
